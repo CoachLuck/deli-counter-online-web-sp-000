8064e4f1725a883f15f51f8c66aa70cff49e79af
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-22 19:07:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 
 def line(arr)
-  arr.length == 0 ? puts "The line is currently empty."
+  (arr.length == 0) ? puts "The line is currently empty."
     : arr.each_with_index {|name, idx| puts "Welcome, #{name}. You are number #{idx + 1} in line."}
 end
 
