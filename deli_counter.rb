653747d19a031560978bf2bbd19e9edc44cc4386
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-22 19:07:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,3 +5,8 @@ def line(arr)
   arr.length == 0 ? puts "The line is currently empty."
     : arr.each_with_index {|name, idx| puts "Welcome, #{name}. You are number #{idx + 1} in line."}
 end
+
+def now_serving(arr)
+  arr.length == 0 ? puts "There is nobody waiting to be served!" :
+    puts arr.shift()
+end
